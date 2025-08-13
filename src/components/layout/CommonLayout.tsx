61d49bee92b254e4f61a2f427b054d984d78436e
Mostary Jahan <155467590+mostaryjahan@users.noreply.github.com>
2025-08-13 12:03:04 +0600

    initial project setup

---
+++ src/components/layout/CommonLayout.tsx
@@ -0,0 +1,21 @@
+import type { ReactNode } from "react";
+
+import Footer from "./Footer";
+import Navbar from "./Navbar";
+
+interface IProps {
+  children: ReactNode;
+}
+
+const CommonLayout = ({ children }: IProps) => {
+  return (
+    <div className="min-h-screen flex flex-col flex-grow">
+      <Navbar />
+
+      <div className="grow-1"> {children}</div>
+      <Footer />
+    </div>
+  );
+};
+
+export default CommonLayout;
---
+++ src/components/modules/aboutPage/AboutHeader.tsx
@@ -0,0 +1,9 @@
+const AboutHeader = () => {
+  return (
+    <div>
+      <h1>This is AboutHeader component</h1>
+    </div>
+  );
+};
+
+export default AboutHeader;

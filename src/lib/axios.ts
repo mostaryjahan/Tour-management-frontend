597f73787c1aabe9d861b9a8ddbe49d42aac1d0d
Mostary Jahan <155467590+mostaryjahan@users.noreply.github.com>
2025-08-22 23:58:59 +0600

    add

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -11,8 +11,7 @@ axiosInstance.interceptors.request.use(
   function (config) {
     const token = localStorage.getItem("accessToken");
     if (token) {
-      config.headers = config.headers || {};
-      config.headers.Authorization = `Bearer ${token}`;
+      config.headers.set('Authorization', `Bearer ${token}`);
     }
 
     return config;
